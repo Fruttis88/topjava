c1db5c973248a4d7e24c3502e38459082750668b
Fruttis88 <STASAN1988@yandex.ru>
2017-01-25 12:47:39 +0300

    HW6_1.3_optional_fix

--- src/main/java/ru/javawebinar/topjava/web/MealController.java
+++ src/main/java/ru/javawebinar/topjava/web/MealController.java
@@ -1,12 +1,10 @@
 package ru.javawebinar.topjava.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
